5cab77c5cbdd2430e7b88b225b6cc5c908ad28bf
Jon <lundbyjon@hotmail.com>
2023-09-14 09:35:04 +0200

    basics for app.js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-"use strict";
+import express from "express";
 
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log("app running on port: 3333")
+});
+
+app.get("/", (req, res) => {
+    res.send("hello world!")
+});
