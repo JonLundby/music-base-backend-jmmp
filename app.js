60d649e1acdd71e62a8c0e87b7e25fa760e834d2
MarkusIngerslev <markusolsen2004@gmail.com>
2023-09-19 15:17:20 +0200

    Fjernet unødvendig kode

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// -------------- ctr+f and search "todo" to find db deviations and other possible corrections -------------- \\
-// -------------- ctr+f and search "todo" to find db deviations and other possible corrections -------------- \\
-// -------------- ctr+f and search "todo" to find db deviations and other possible corrections -------------- \\
-
+// ===== IMPORTS ===== //
 import express from "express";
 import fs from "fs/promises";
 import cors from "cors";
 import connection from "./database.js";
+// Imports from routes
+import tracksRouter from "./routes/tracks.js";
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -13,84 +12,15 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 app.use(cors());
 
+// ROUTERS
+app.use("/tracks", tracksRouter);
+
 // ----- MAIN GET ----- \\
 app.get("/", (req, res) => {
-  res.send("hello MusicBase!");
-});
-
-// ----- ARTISTS ROUTES ----- \\
-// GET all artists
-app.get("/artists", async (request, response) => {
-  const query = "SELECT * FROM artists";
-  connection.query(query, (err, results, fields) => {
-    if (err) {
-      console.log(err);
-    } else {
-      response.json(results);
-    }
-  });
-});
-
-// GET specific artist
-app.get("/artists/:id", async (request, response) => {
-  const id = request.params.id;
-  const query = "SELECT * FROM artists WHERE id=?;";
-  const values = [id];
-  connection.query(query, values, (err, results, fields) => {
-    if (err) {
-      console.log(err);
-    } else {
-      response.json(results[0]); //[0] makes the return a single object instead of array with one object
-    }
-  });
-});
-
-// CREATE artist
-app.post("/artists", async (request, response) => {
-  const artist = request.body;
-  const query = "INSERT INTO artists(name, birthdate) values(?, ?);"; //todo add relevant properties
-  const values = [artist.name, artist.birthdate]; //todo add relevant properties
-
-  connection.query(query, values, (err, results, fields) => {
-    if (err) {
-      console.log(err);
-    } else {
-      response.json(results);
-    }
-  });
-});
-
-// UPDATE artist
-app.put("/artists/:id", async (request, response) => {
-  const id = request.params.id;
-  const user = request.body;
-  const query = "UPDATE artists SET name=?, birthdate=? WHERE id=?"; //todo add relevant properties
-  const values = [user.name, user.birthdate, id]; //todo add relevant properties
-
-  connection.query(query, values, (err, results, fields) => {
-    if (err) {
-      console.log(err);
-    } else {
-      response.json(results);
-    }
-  });
-});
-
-//DELETE artists
-app.delete("/artists/:id", async (request, response) => {
-  const id = request.params.id;
-  const query = "DELETE FROM artists WHERE id=?;";
-  const values = [id];
-  connection.query(query, values, (err, results, fields) => {
-    if (err) {
-      console.log(err);
-    } else {
-      response.json(results);
-    }
-  });
+    res.send("Node express MusicBase REST API... Running 🎉");
 });
 
 // ----- PORT LISTENER ----- \\
 app.listen(port, () => {
-  console.log("app running on port: 3333");
+    console.log(`app running on port: http://localhost:${port}`);
 });
