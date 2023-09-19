dc4d65738406f7d8a6c6e8e02bc47673a6dfca47
MarkusIngerslev <markusolsen2004@gmail.com>
2023-09-19 13:40:48 +0200

    Ændret i måde man tilgår database, så det er gennem .env

--- database.js
+++ database.js
@@ -1,14 +1,14 @@
 // database name and password to be replaced
-
 import mysql2 from "mysql2";
+import "dotenv/config";
 
 const connection = mysql2.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  database: "artists_db", // todo set db name
-  password: "", //todo set password
-  multipleStatements: true,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
+    multipleStatements: true,
 });
 
 export default connection;
