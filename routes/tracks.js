a2946be2251d4249a4084d3db87d5e5a619eea13
MarkusIngerslev <markusolsen2004@gmail.com>
2023-09-19 18:55:17 +0200

    Tilpasset forskellige get's så det passer bedre til vores database

--- routes/tracks.js
+++ routes/tracks.js
@@ -3,16 +3,18 @@ import connection from "../database.js";
 
 const tracksRouter = Router();
 
-// ROUTER GET "/tracks" - get all tracks
+// GET Endpoint "/tracks" - get all songs with artist information
 tracksRouter.get("/", (req, res) => {
     const queryString = /*sql*/ `
-        SELECT tracks.*,
+        SELECT tracks.trackID,
+            tracks.trackName,
+            tracks.duration,
             artists.name AS artistName,
-            artists.artistId AS artistId,
-            artists.genres AS artistGenre
+            artists.artistID AS artistID,
+            artists.genres AS artistGenres
         FROM tracks
-        INNER JOIN albums ON tracks.albumId = albums.albumId
-        INNER JOIN artists ON albums.artistId = artists.artistId;  
+        INNER JOIN track_artists ON tracks.trackID = track_artists.trackID
+        INNER JOIN artists ON track_artists.artistID = artists.artistID;  
     `;
 
     connection.query(queryString, (err, results) => {
@@ -25,26 +27,30 @@ tracksRouter.get("/", (req, res) => {
     });
 });
 
-// ROUTER GET "/tracks/:id" - get specific song
+// GET Endpoint "/tracks/:id" - get specific song by ID
 tracksRouter.get("/:id", (req, res) => {
     const id = req.params.id;
     const queryString = /*sql*/ `
-        SELECT tracks.*,
-            albums.albumTitle AS albumTitle,
-            artists.name AS artistName,
-            artists.artistId AS artistId,
-            artists.genres AS artistGenre
+        SELECT tracks.trackID,
+               tracks.trackName,
+               tracks.duration,
+               albums.albumTitle AS albumTitle,
+               artists.artistID AS artistID,
+               artists.name AS artistName,
+               artists.genres AS artistGenres
         FROM tracks
-        INNER JOIN albums ON tracks.albumId = albums.albumId
-        INNER JOIN artists ON albums.artistId = artists.artistId
-        WHERE tracks.trackId = ?;
+        INNER JOIN track_albums ON tracks.trackID = track_albums.trackID
+        INNER JOIN albums ON track_albums.albumID = albums.albumID
+        INNER JOIN album_artists ON albums.albumID = album_artists.albumID
+        INNER JOIN artists ON album_artists.artistID = artists.artistID
+        WHERE tracks.trackID = ?;
     `;
     const values = [id];
 
     connection.query(queryString, values, (err, results) => {
         if (err) {
             console.error(err);
-            res.status(500).json({ error: "Der opstid en fejl ved forespørgslen." });
+            res.status(500).json({ error: "Der opstod en fejl under forespørgslen." });
         } else {
             if (results.length === 0) {
                 res.status(404).json({ error: "Sangen blev ikke fundet." });
