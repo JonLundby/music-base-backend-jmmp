23279cf74e273a45f1a7cc654fea995499d60488
Jon <lundbyjon@hotmail.com>
2023-09-20 15:17:50 +0200

    mysql2 -> mysql

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 // database name and password to be replaced
-import mysql2 from "mysql2";
+import mysql from "mysql2";
 import "dotenv/config";
 
-const connection = mysql2.createConnection({
+const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
