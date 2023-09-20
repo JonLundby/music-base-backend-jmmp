0a9cecadd20a8c7943afed931479f8c2ee2e5b72
Jon <lundbyjon@hotmail.com>
2023-09-20 18:20:07 +0200

    removed some comments

--- routes/albums.js
+++ routes/albums.js
@@ -3,32 +3,8 @@ import connection from "../database.js";
 
 const albumsRouter = Router();
 
-// get all albums (MOST LIKELY NOT WORKING PROPERLY)
-// albumsRouter.get("/", (request, response) => {
-//     const queryString = /*sql*/ `
-//             SELECT DISTINCT albums.*,
-//                 artists.name AS artistName,
-//                 artists.artistID AS artistId
-//             FROM albums
-//             JOIN track_albums ON albums.albumID = track_albums.albumID
-//             JOIN tracks ON track_albums.trackID = tracks.trackID
-//             JOIN track_artists ON tracks.trackID = track_artists.trackID
-//             JOIN artists ON track_artists.artistID = artists.artistID;
-//     `;
-//     connection.query(queryString, (error, results) => {
-//       if (error) {
-//         console.log(error);
-//       } else {
-//         response.json(results);
-//       }
-//     });
-// });
-
-//get single album
-
-//get single album with songs
-
-//############ get albums test start ############ \\
+//CRUD
+// ----- ALBUM ROUTES ----- \\
 // GET Endpoint "/albums" - get all albums with artist information
 albumsRouter.get("/", (req, res) => {
   const queryString = /*sql*/ `
@@ -63,8 +39,8 @@ albumsRouter.get("/", (req, res) => {
   });
 });
 
-// GET Endpoint "/tracks/:id" - get specific song by ID
-// tracksRouter.get("/:id", (req, res) => {
+// GET Endpoint "/albums/:id" - get specific album by ID
+// albumsRouter.get("/:id", (req, res) => {
 //     const id = req.params.id;
 //     const queryString = /*sql*/ `
 //         SELECT tracks.trackID,
@@ -101,36 +77,6 @@ albumsRouter.get("/", (req, res) => {
 //         }
 //     });
 // });
-
-//############ get albums test slut ############ \\
-
-//CRUD
-// ----- ALBUM ROUTES ----- \\
-// GET all albums
-albumsRouter.get("/", async (request, response) => {
-  const query = "SELECT * FROM albums;";
-  connection.query(query, (err, results, fields) => {
-    if (err) {
-      console.log(err);
-    } else {
-      response.json(results);
-    }
-  });
-});
-
-// GET specific album
-albumsRouter.get("/:id", async (request, response) => {
-  const id = request.params.id;
-  const query = "SELECT * FROM albums WHERE albumID=?;";
-  const values = [id];
-  connection.query(query, values, (err, results, fields) => {
-    if (err) {
-      console.log(err);
-    } else {
-      response.json(results[0]); //[0] makes the return a single object instead of array with one object
-    }
-  });
-});
 
 // CREATE album
 albumsRouter.post("/", async (request, response) => {
@@ -176,6 +122,33 @@ albumsRouter.delete("/:id", async (request, response) => {
     }
   });
 });
+
+
+// GET all albums
+// albumsRouter.get("/", async (request, response) => {
+//   const query = "SELECT * FROM albums;";
+//   connection.query(query, (err, results, fields) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       response.json(results);
+//     }
+//   });
+// });
+
+// GET specific album
+// albumsRouter.get("/:id", async (request, response) => {
+//   const id = request.params.id;
+//   const query = "SELECT * FROM albums WHERE albumID=?;";
+//   const values = [id];
+//   connection.query(query, values, (err, results, fields) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       response.json(results[0]); //[0] makes the return a single object instead of array with one object
+//     }
+//   });
+// });
 
 // ----- EXPORTS ----- \\
 export default albumsRouter;
