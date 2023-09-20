0ddac6ca73dc3f791bafbaefb486460e3bc050b4
Jon <lundbyjon@hotmail.com>
2023-09-20 17:26:22 +0200

    testing albumsRouter.get("/")...

--- routes/albums.js
+++ routes/albums.js
@@ -30,7 +30,81 @@ const albumsRouter = Router();
 
 //get single album with songs
 
+//############ get albums test start ############ \\
+// GET Endpoint "/albums" - get all albums with artist information
+albumsRouter.get("/", (req, res) => {
+  const queryString = /*sql*/ `
+        SELECT albums.albumID,
+               albums.albumCover,
+               albums.releaseDate,
+               albums.abumTitle,
+               albums.numberofTracks
+               GROUP_CONCAT(artists.name) AS artistNames,
+               GROUP_CONCAT(artists.artistID) AS artistIDs,
+               GROUP_CONCAT(artists.genres) AS artistGenres
+        FROM albums
+        INNER JOIN album_artists ON tracks.albumID = album_artists.albumID
+        INNER JOIN artists ON album_artists.artistID = artists.artistID
+        GROUP BY albums.albumID, albums.albumCover, albums.releaseDate, albums.abumTitle, albums.numberofTracks;
+    `;
 
+  connection.query(queryString, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: "Der opstod en fejl ved forespørgslen." });
+    } else {
+      // Process the artistNames, artistIDs, and artistGenres into arrays
+      results.forEach((result) => {
+        result.artistNames = result.artistNames.split(",");
+        result.artistIDs = result.artistIDs.split(",").map(Number);
+        result.artistGenres = result.artistGenres.split(",");
+      });
+
+      res.json(results);
+    }
+  });
+});
+
+// GET Endpoint "/tracks/:id" - get specific song by ID
+// tracksRouter.get("/:id", (req, res) => {
+//     const id = req.params.id;
+//     const queryString = /*sql*/ `
+//         SELECT tracks.trackID,
+//                tracks.trackName,
+//                tracks.duration,
+//                albums.albumTitle AS albumTitle,
+//                GROUP_CONCAT(artists.artistID) AS artistIDs,
+//                GROUP_CONCAT(artists.name) AS artistNames,
+//                GROUP_CONCAT(artists.genres) AS artistGenres
+//         FROM tracks
+//         INNER JOIN track_albums ON tracks.trackID = track_albums.trackID
+//         INNER JOIN albums ON track_albums.albumID = albums.albumID
+//         INNER JOIN track_artists ON tracks.trackID = track_artists.trackID
+//         INNER JOIN artists ON track_artists.artistID = artists.artistID
+//         WHERE tracks.trackID = ?
+//         GROUP BY tracks.trackID, tracks.trackName, tracks.duration, albums.albumTitle;
+//     `;
+//     const values = [id];
+
+//     connection.query(queryString, values, (err, results) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).json({ error: "Der opstod en fejl under forespørgslen." });
+//         } else {
+//             if (results.length === 0) {
+//                 res.status(404).json({ error: "Sangen blev ikke fundet." });
+//             } else {
+//                 const trackInfo = results[0];
+//                 trackInfo.artistIDs = trackInfo.artistIDs.split(",").map(Number);
+//                 trackInfo.artistNames = trackInfo.artistNames.split(",");
+//                 trackInfo.artistGenres = trackInfo.artistGenres.split(",");
+//                 res.json(trackInfo);
+//             }
+//         }
+//     });
+// });
+
+//############ get albums test slut ############ \\
 
 //CRUD
 // ----- ALBUM ROUTES ----- \\
