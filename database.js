a4b294d399483ac152a9f01d0230f42f92eb5e3d
Jon <lundbyjon@hotmail.com>
2023-09-21 12:52:52 +0200

    ports enabled

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ import fs from "fs/promises";
 
 const dbConfig = {
   host: process.env.MYSQL_HOST,
-  // port: process.env.MYSQL_PORT,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   database: process.env.MYSQL_DATABASE,
   password: process.env.MYSQL_PASSWORD,
