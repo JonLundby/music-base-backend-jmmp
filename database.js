9c1ea5aae05da7123b3e9d4ac397e13c5ecc5cba
MudD3RTv <markusolsen2004@gmail.com>
2023-09-22 09:23:44 +0200

    Tilføjet error handling til connection

--- database.js
+++ database.js
@@ -4,26 +4,26 @@ import "dotenv/config";
 import fs from "fs/promises";
 
 const dbConfig = {
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  database: process.env.MYSQL_DATABASE,
-  password: process.env.MYSQL_PASSWORD,
-  multipleStatements: true,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
+    multipleStatements: true,
 };
 
 if (process.env.MYSQL_CERT) {
-  dbConfig.ssl = { ca: await fs.readFile("DigiCertGlobalRootCA.crt.pem") };
+    dbConfig.ssl = { ca: await fs.readFile("DigiCertGlobalRootCA.crt.pem") };
 }
 
 const connection = mysql.createConnection(dbConfig);
 
-// connection.connect((err) => {
-//     if (err) {
-//         console.error("Fejl ved oprettelse af forbindelse til database: " + err);
-//     } else {
-//         console.log("Forbindelse til database er oprettet.");
-//     }
-// });
+connection.connect((err) => {
+    if (err) {
+        console.error("Fejl ved oprettelse af forbindelse til database: " + err);
+    } else {
+        console.log("Forbindelse til database er oprettet.");
+    }
+});
 
 export default connection;
